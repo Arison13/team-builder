1c4c1c8c6519c98b546556e1b9da21d3275e0b82
Arison13 <arisonarias@gmail.com>
2021-09-14 15:19:07 -0400

    basic form is set up

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,8 +1,41 @@
+import React, {useState} from "react";
+
+
 function Form (){
 
 
     return (
-        <h1>WORK IN PROGRESS!</h1>
+        <div className='form'> 
+            <form>
+                <label>Name
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Enter Your Name"
+                    value=''
+                />
+                </label>
+
+                <label>Email
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter Your Email"
+                    value=''
+                />
+                </label>
+                <label> Role
+                    <select name='role'>
+                        <option>--Pick a Role--</option>
+                        <option>BackEnd Engineer</option>
+                        <option>FronEnd Engineer</option>
+                        <option>Designer</option>
+                        <option>Data Scientist</option>
+                    </select>
+                </label>
+
+            </form>
+        </div>
     )
 }
 export default Form;
