533c01b50a90123a6bd302bab750e8c501b36fc7
Arison13 <arisonarias@gmail.com>
2021-09-15 00:08:44 -0400

    i can change the name input, still working on the email input, i need to be hook the email and the role input so that it concats and displays on the friends array

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,18 +1,49 @@
 import React, {useState} from "react";
 
+const initialFormValues = { friendName: '', friendEmail: '', friendRole: ''}
 
-function Form (){
+const friendsList =[
+    {friendName: 'Ari', friendEmail: 'ari@ari.com', friendRole: 'FrontEnd-Developer'},
+]
 
-
+function Form () {
+ const [formValues, setFormValues]= useState(initialFormValues);
+ const [friends, setFriends]=useState(friendsList);
+ const change = (evt) => {
+    // const { name, value, } = evt.target;
+    const name = evt.target.name;
+    const value = evt.target.value;
+    setFormValues({ ...formValues, [name]: value });
+  }
+  
+  const submit = (evt) => {
+    evt.preventDefault();
+    const newFriend = {
+      friendName: formValues.friendName,
+      friendEmail: formValues.friendEmail,
+      friendRole: formValues.friendRole
+    }
+    
+    setFriends(friends.concat(newFriend));
+    setFormValues(initialFormValues);
+  }
+ 
     return (
         <div className='form'> 
-            <form>
+            <h2>Friend Form </h2>
+            {friends.map((friend, idx) => (
+                <div key={idx}>
+                   <p> Friend Name is: {friend.friendName}: Email: {friend.friendEmail} Role: {friend.friendRole} </p>
+                </div>
+            ))}
+            <form onSubmit={submit}>
                 <label>Name
                 <input
                     type="text"
-                    name="name"
+                    name="friendName"
                     placeholder="Enter Your Name"
-                    value=''
+                    value={formValues.friendName}
+                    onChange={change}
                 />
                 </label>
 
@@ -21,7 +52,8 @@ function Form (){
                     type="email"
                     name="email"
                     placeholder="Enter Your Email"
-                    value=''
+                    value={formValues.friendEmail}
+                    onChange={change}
                 />
                 </label>
                 <label> Role
@@ -33,7 +65,8 @@ function Form (){
                         <option>Data Scientist</option>
                     </select>
                 </label>
-
+            
+                <input type='submit' value='Become A Friend' />
             </form>
         </div>
     )
